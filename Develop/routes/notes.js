53641fbc434b06ded7f7e3f2ab3cd10db6a59a57
Robert Parsons <rmpcoding@gmail.com>
2020-05-01 14:21:56 -0700

    Adds api.POST method to create a unique ID to the note, as well as attaching the note to the JSON file. So far GET and POST work now

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -4,23 +4,60 @@ const fs = require('fs');
 
 const api = express.Router();
 
-
 api.get('/api/notes', (req, res, next) => {
-  res.json(db)
+    res.json(db);
 });
 
-// POST /api/notes - Should recieve a new note to save on the request body, add it to the db.json file, and then return the new note to the client.
 api.post('/api/notes', (req, res, next) => {
-  console.log('POSTed something yay!')
-})
+    let resultsArr = [];
+    let note;
+    let id;
 
-// DELETE /api/notes/:id - Should recieve a query paramter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
+    for (let i = 0; i < 1; i++) {
+        if (db[i]) {
+            resultsArr = db;
+            id = db[db.length - 1].id;
+            id += 1;
+            req.body.id = id;
+            note = req.body;
+            resultsArr.push(note);
+        } else {
+            req.body.id = 0;
+            note = req.body;
+            resultsArr.push(note);
+        }
+    }
+
+    const json = JSON.stringify(resultsArr, null, 2);
+
+    fs.writeFile('./db/db.json', json, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('You have successfully written the json file.');
+        }
+    });
+
+});
+
+// DELETE /api/notes/:id - Should recieve a query parameter containing the id of a note to delete. This means you'll need to find a way to give each note a unique id when it's saved. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.
 api.delete('/api/notes/:id', (req, res, next) => {
-  fs.readFile('./db/db.json', (err, data) => {
-    if (err) throw err;
-    console.log(db[0]) //iterate through the array using for loop;
-  });
-  console.log('DELETEd yay!')
-})
+    fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        for (let i = 0; i < db.length; i++) {
+            id = req.params.id
+            db.splice(id, 1)
+            console.log(db)
+        }
+        fs.writeFile('./db/db.json', db, 'utf8', (err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('You have successfully written the json file.');
+            }
+        });
+    });
+    console.log('DELETEd yay!');
+});
 
 module.exports = api;
