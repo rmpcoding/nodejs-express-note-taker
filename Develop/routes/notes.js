7905ed10074d6f2f7ab7f1d1bb345a33a643e0ea
Robert Parsons <rmpcoding@gmail.com>
2020-05-03 01:25:29 -0700

    Adds writeFile function for a more DRY approach. Still need to fix the issue of the response not sending DB after the file has been written

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -3,6 +3,19 @@ const db = require('../db/db.json');
 const fs = require('fs');
 
 const api = express.Router();
+const app = express();
+
+
+const writeFile = (data) => {
+    fs.writeFile('./db/db.json', data, 'utf8', (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('You have successfully written the json file.');
+        }
+    });
+};
+
 
 api.get('/api/notes', (req, res, next) => {
     res.json(db);
@@ -29,14 +42,9 @@ api.post('/api/notes', (req, res, next) => {
     }
 
     const json = JSON.stringify(resultsArr, null, 2);
-
-    fs.writeFile('./db/db.json', json, 'utf8', (err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            console.log('You have successfully written the json file.');
-        }
-    });
+    console.log('before')
+    writeFile(json);
+    console.log('after')
 });
 
 api.delete('/api/notes/:id', (req, res, next) => {
@@ -48,26 +56,20 @@ api.delete('/api/notes/:id', (req, res, next) => {
 
         id = parseInt(req.params.id);
 
-
         // for loop iterates over array to assign unique id after note is deleted
         for (let i = id; i < db.length; i++) {
             resultsArr[i].id -= 1;
-            console.log(resultsArr)
         }
 
         resultsArr.splice(id, 1);
-        console.log(resultsArr)
         const json = JSON.stringify(resultsArr, null, 2);
 
-        fs.writeFile('./db/db.json', json, 'utf8', (err) => {
-            if (err) {
-                console.log(err);
-            } else {
-                console.log('You have successfully written the json file.');
-            }
-        });
+        console.log('before');
+        writeFile(json);
+        console.log('after');
     });
-    console.log('DELETEd yay!');
 });
 
 module.exports = api;
+
+// Figure out what's keeping the notes from appearing/disappearing before page refresh
