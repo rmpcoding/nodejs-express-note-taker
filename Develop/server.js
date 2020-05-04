b8cb8a2372f11de5b3bf8c7943974bd8b204f547
Robert Parsons <rmpcoding@gmail.com>
2020-05-04 01:51:53 -0700

    Removes unnecessary dependencies

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes/index');
 const api = require('./routes/notes');
-const static = require('node-static');
 
 const app = express();
 
@@ -12,17 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// Static CSS for deployment
-const file = new static.Server('./public');
-
-const staticListen = (req, res) => {
-    req.addListener('end', function() {
-        file.serve(req, res);
-    }).resume();
-}
-
-staticListen();
 
 // Custom routes (html index, notes api) middleware
 // __________________________________________
